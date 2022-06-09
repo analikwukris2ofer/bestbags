e75e246b74df852dfcf454861632ce860c6bb07f
Christopher Analikwu <analikwuchristopher@gmail.com>
2022-06-09 03:58:20 +0200

    build the image slider

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import winter from "./images/wintershop.png";
+import autumn from "./images/autumnshop.png";
+import summer from "./images/summershop.png";
+
+export const SliderItems = [
+  {
+    id: 1,
+    img: summer,
+    title: "SUMMER SALE",
+    desc: "GET 40% OFF ON NEW ARRIVALS. SUMMER SALE!!",
+    bg: "f5fafc",
+  },
+  {
+    id: 2,
+    img: winter,
+    title: "WINTER SALE",
+    desc: "GET 40% OFF ON NEW ARRIVALS. SUMMER SALE!!",
+    bg: "f5fafc",
+  },
+  {
+    id: 3,
+    img: autumn,
+    title: "AUTUMN SALE",
+    desc: "GET 40% OFF ON NEW ARRIVALS. SUMMER SALE!!",
+    bg: "f5fafc",
+  },
+];
