841505fc64900f7420f7ce9f27eb8a4595eb7ffd
Christopher Analikwu <analikwuchristopher@gmail.com>
2022-06-14 14:59:53 +0200

    building the login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
-  return <Product />;
+  return <Login />;
 }
 
 export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components";
+import image from "../images/wintershop.png";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://img.freepik.com/free-psd/black-woman-holding-shopping-bags_1187-28989.jpg?w=2000")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By Creating this account, i willingly agree to abide by all the site
+            rules and regulations😊
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
